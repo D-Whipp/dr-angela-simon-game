a2cd64e87f5fb35e3fe18bf81459313d5a925f93
David Whipple <dwhipp88@gmail.com>
2024-05-07 17:36:45 -0600

    added logic to determine the first time a key is pressed and update the title with the level,added logic to update the level everytime nextSequence is called

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 let buttonColors = ['red', 'blue', 'green', 'yellow'];
 let gamePattern = [];
 let userClickedPattern = [];
+let level = 0;
+let started = false;
 
 function playSound(name) {
     let audio = new Audio('./sounds/' + name + '.mp3');
@@ -22,6 +24,9 @@ $('.btn').click(function () {
 });
 
 function nextSequence() {
+    level++;
+    $('#level-title').text('Level ' + level);
+
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
@@ -34,4 +39,18 @@ function nextSequence() {
     playSound(randomChosenColor);
 }
 
-nextSequence();
+// if (started === false) {
+//     $(document).on('keypress', function () {
+//         $('#level-title').text('Level ' + level);
+//         nextSequence();
+//     });
+//     started = true;
+// }
+
+$(document).keypress(function () {
+    if (!started) {
+        $('#level-title').text('Level ' + level);
+        nextSequence();
+        started = true;
+    }
+});
