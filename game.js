7782543942f7257496b312964712fb0fb71cc05a
David Whipple <dwhipp88@gmail.com>
2024-05-07 17:17:04 -0600

    added animations to user clicks

--- game.js
+++ game.js
@@ -2,19 +2,22 @@ let buttonColors = ['red', 'blue', 'green', 'yellow'];
 let gamePattern = [];
 let userClickedPattern = [];
 
-// 1. In the same way we played sound in nextSequence() , when a user clicks on a button, the corresponding sound should be played. e.g if the Green button is clicked, then green.mp3 should be played.
-
-// 2. Create a new function called playSound() that takes a single input parameter called name.
 function playSound(name) {
-    // 3. Take the code we used to play sound in the nextSequence() function and move it to playSound().
-    // 4. Refactor the code in playSound() so that it will work for both playing sound in nextSequence() and when the user clicks a button.
     let audio = new Audio('./sounds/' + name + '.mp3');
     audio.play();
+}
+
+function animatePress(currentColor) {
+    $('#' + currentColor).addClass('pressed');
+    setTimeout(function () {
+        $('#' + currentColor).removeClass('pressed');
+    }, 100);
 }
 
 $('.btn').click(function () {
     let userChosenColor = $(this).attr('id');
     playSound(userChosenColor);
+    animatePress(userChosenColor);
     userClickedPattern.push(userChosenColor);
 });
 
