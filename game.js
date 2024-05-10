12b4fe77f08b9e015a1719bcebc7e80a5f6096ca
David Whipple <dwhipp88@gmail.com>
2024-05-09 19:01:29 -0600

    added game over logic for when the user picks the wrong color

--- game.js
+++ game.js
@@ -37,7 +37,13 @@ function checkAnswer(currentLevel) {
             }, 1000);
         }
     } else {
-        console.log('Wrong!');
+        playSound('wrong');
+        $('body').addClass('game-over');
+        setTimeout(function () {
+            $('body').removeClass('game-over');
+        }, 200);
+
+        $('#level-title').text('Game Over, Press Any Key to Restart');
     }
 }
 
